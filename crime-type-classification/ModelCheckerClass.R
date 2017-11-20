81ed1fac4614b336a984c69c0e3db361aeb1515f
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2017-11-20 23:39:09 +0100

    cross validation

---
+++ crime-type-classification/ModelCheckerClass.R
@@ -0,0 +1,49 @@
+library(caret)
+source(file="ClassificationModelClass.R")
+
+ModelCheckerClass <- setRefClass(
+  Class="ModelCheckerClass",
+  fields=list(
+    data="data.frame",
+    classificationModel="ClassificationModelClass"
+  ),
+  methods = list(
+    init = function(crimeDataClass, classificationModel) {
+      crimeDataClass$init()
+      data <<- crimeDataClass$getData()
+      classificationModel <<- classificationModel
+    },
+    normalChecking = function() {
+      smp_size <- floor(0.8 * nrow(data))
+      train_ind <- sample(seq_len(nrow(data)), size = smp_size)
+      train <- data[train_ind, ]
+      test <- data[-train_ind, ]
+      
+      classificationModel$trainModel(train)
+      results <- classificationModel$predictLabels(getDataWithoutLabels(test))
+    },
+    crossValidation = function(k = 10) {
+      set.seed(123)
+      folds <- createFolds(factor(data$label), k = k, list = FALSE)
+      
+      categories <- unique(data$label)
+      results <- data.frame(matrix(NA, nrow = 0, ncol = length(categories)))
+      colnames(results) <- categories
+      
+      for(i in 1:k) {
+        train <- data[folds != i, ]
+        test <- data[folds == i, ]
+        
+        logisticRegressionModelClass <- LogisticRegressionModelClass()
+        logisticRegressionModelClass$trainModel(train)
+        testWithoutLabels <- test[, -which(names(test) == "label")]
+        results <- rbind(results ,logisticRegressionModelClass$predictLabels(testWithoutLabels))
+      }
+      
+      results
+    },
+    getDataWithoutLabels = function(data) {
+      data[, -which(names(data) == "label")]
+    }
+  )
+)
--- crime-type-classification/main.R
+++ crime-type-classification/main.R
@@ -1,13 +1,20 @@
+library(caret)
 
 source(file="BialystokCrimeDataClass.R")
 source(file="LogisticRegressionModelClass.R")
+source(file="ModelCheckerClass.R")
 
 bialystokCrimeDataClass <- BialystokCrimeDataClass()
+logisticRegressionModelClass <- LogisticRegressionModelClass()
+modelChekcerClass <- ModelCheckerClass()
+modelChekcerClass$init(bialystokCrimeDataClass, logisticRegressionModelClass)
+#results <- modelChekcerClass$normalChecking()
+results <- modelChekcerClass$crossValidation()
+
 bialystokCrimeDataClass$init()
 
 data <- bialystokCrimeDataClass$getData()
 
-set.seed(123)
 smp_size <- floor(0.8 * nrow(data))
 train_ind <- sample(seq_len(nrow(data)), size = smp_size)
 train <- data[train_ind, ]
@@ -15,9 +22,27 @@ test <- data[-train_ind, ]
 
 logisticRegressionModelClass <- LogisticRegressionModelClass()
 logisticRegressionModelClass$trainModel(train)
-
 results <- logisticRegressionModelClass$predictLabels(test[, c("lat", "lng", "date")])
 
+k = 10
+set.seed(123)
+folds <- createFolds(factor(data$label), k = k, list = FALSE)
+
+categories <- unique(data$label)
+results <- data.frame(matrix(NA, nrow = 0, ncol = length(categories)))
+colnames(results) <- categories
+
+for(i in 1:k) {
+  train <- data[folds != i, ]
+  test <- data[folds == i, ]
+  
+  logisticRegressionModelClass <- LogisticRegressionModelClass()
+  logisticRegressionModelClass$trainModel(train)
+  testWithoutLabels <- test[, -which(names(test) == "label")]
+  results <- rbind(results ,logisticRegressionModelClass$predictLabels(testWithoutLabels))
+}
+
+testWithoutLabels <- test[, -which(names(test) == "label")]
 
 pr <- prediction(results, test$label)
 prf <- performance(pr, measure = "tpr", x.measure = "fpr")
